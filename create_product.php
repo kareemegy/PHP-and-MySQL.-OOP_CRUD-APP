f960b16141f774d1bbce39db2e9c724b94a1f960
karim <kokekarim@yahoo.com>
2019-07-13 21:14:39 +0200

    END CRUD FEATURES

--- create_product.php
+++ create_product.php
@@ -1,6 +1,6 @@
 <?php
 //include database and object files
-include_once '../config/database.php';
+include_once 'config/database.php';
 include_once 'objects/product.php';
 include_once 'objects/category.php';
 
@@ -17,7 +17,9 @@ $page_title = "Create Product";
 include_once "layout_header.php";
 
 echo "<div class='right-button-margin'>";
-echo "<a href='index.php' class='btn btn-default pull-right'>Read Products</a>";
+echo "<a href='index.php' class='btn btn-primary pull-right'>";
+echo "<span class='glyphicon glyphicon-list'></span> Read Products";
+echo "</a>";
 echo "</div>";
 
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 // set page header
 $page_title = "Read Products";
-include_once "PHP_CRUD_OOP/layout_header.php";
+include_once "layout_header.php";
 
 echo "<div class='right-button-margin'>";
-echo "<a href='PHP_CRUD_OOP/create_product.php' class='btn btn-default pull-right'>Create Product</a>";
+echo "<a href='create_product.php' class='btn btn-default pull-right'>Create Product</a>";
 echo "</div>";
 
 //page given in URL parameter, default page is one
@@ -18,8 +18,8 @@ $from_record_num = ($records_per_page * $page) - $records_per_page;
 
 // include database and object files
 include_once 'config/database.php';
-include_once 'PHP_CRUD_OOP/objects/product.php';
-include_once 'PHP_CRUD_OOP/objects/category.php';
+include_once 'objects/product.php';
+include_once 'objects/category.php';
 
 // instantiate database and objects
 $database = new Database();
@@ -99,4 +99,4 @@ else {
 }
 
 // set page footer
-include_once "PHP_CRUD_OOP/layout_footer.php";
+include_once "layout_footer.php";
