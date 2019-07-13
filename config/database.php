21395f9132e055e4f3f52b647b6c17341c671f82
karim <kokekarim@yahoo.com>
2019-07-13 20:45:08 +0200

    start app & and some features

---
+++ config/database.php
@@ -0,0 +1,23 @@
+<?php
+class Database
+{
+    // specify your own database credentials
+    private $host = "localhost";
+    private $db_name = "php_oop_crud_level_1";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection()
+    {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+
+        } catch (PDOException $exception) {
+            echo "CONNECTION ERRO: " . $exception->getMessage();
+        }
+        return $this->conn;
+
+    }
+}
